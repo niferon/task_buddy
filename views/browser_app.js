90f341ae0e250d7314fcbd3a0eed4a7b762216fe
Oleksandr <alexsavchenko99@gmail.com>
2023-06-24 19:38:33 +0300

    MVC implement, general app restracture.

---
+++ views/browser_app.js
@@ -0,0 +1,5 @@
+const tasksDOM = document.getElementById('tasks');
+const loadingDOM = document.getElementById('loading-text');
+const formDOM = document.getElementById('task-form');
+const taskInputDOM = document.getElementById('task-input');
+const formAlertDOM = document.getElementById('form-alert');
